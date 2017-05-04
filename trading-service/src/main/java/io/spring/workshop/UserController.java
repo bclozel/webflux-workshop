4cf6a17872ffc81df3f9d887bf3c08e8a708e825
Brian Clozel <bclozel@pivotal.io>
2017-05-04 23:03:42 +0200

    Turn UserController into a RestController

--- trading-service/src/main/java/io/spring/workshop/UserController.java
+++ trading-service/src/main/java/io/spring/workshop/UserController.java
@@ -4,12 +4,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
 	private final TradingUserRepository tradingUserRepository;
@@ -18,17 +17,14 @@ public class UserController {
 		this.tradingUserRepository = tradingUserRepository;
 	}
 
-	@GetMapping(path="/users", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
+	@GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Flux<TradingUser> listUsers() {
 		return this.tradingUserRepository.findAll();
 	}
 
-	@GetMapping(path="/users/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
+	@GetMapping(path = "/users/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Mono<TradingUser> showUsers(@PathVariable String username) {
 		return this.tradingUserRepository.findByUserName(username);
 	}
-
 
 }
