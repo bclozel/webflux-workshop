fcd1f9d718c6b547eb73619c01c5ce37c740d1d0
Brian Clozel <bclozel@pivotal.io>
2017-05-09 23:38:38 +0200

    Create trading-service app with start.spring.io

---
+++ trading-service/src/main/java/io/spring/workshop/tradingservice/TradingServiceApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.tradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingServiceApplication.class, args);
+	}
+}
---
+++ trading-service/src/test/java/io/spring/workshop/tradingservice/TradingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.workshop.tradingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
