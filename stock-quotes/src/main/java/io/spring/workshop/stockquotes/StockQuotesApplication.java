6c6c22640a4ec7efe6bf68428dcdb26efc4ace15
Brian Clozel <bclozel@pivotal.io>
2017-05-09 22:05:55 +0200

    Create stock-quotes application on start.spring.io
    We've created a Spring Boot application with the following:
    * Spring Boot 2.0.0 SNAPSHOT
    * Devtools and WebFlux starters

---
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/StockQuotesApplication.java
@@ -0,0 +1,12 @@
+package io.spring.workshop.stockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockQuotesApplication.class, args);
+	}
+}
