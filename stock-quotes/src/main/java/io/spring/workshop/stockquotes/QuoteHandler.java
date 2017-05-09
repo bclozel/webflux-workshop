db4f7fff32458154d0486a4965ea683ee9b87a45
Brian Clozel <bclozel@pivotal.io>
2017-05-09 23:28:30 +0200

    Expose the Flux<Quotes> as a web service

--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
@@ -1,5 +1,6 @@
 package io.spring.workshop.stockquotes;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.stereotype.Component;
@@ -7,11 +8,20 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static java.time.Duration.ofMillis;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class QuoteHandler {
+
+	private final Flux<Quote> quoteStream;
+
+	public QuoteHandler(QuoteGenerator quoteGenerator) {
+		this.quoteStream = quoteGenerator.fetchQuoteStream(ofMillis(1000)).share();
+	}
 
 	public Mono<ServerResponse> hello(ServerRequest request) {
 		return ok().contentType(TEXT_PLAIN)
@@ -21,5 +31,18 @@ public class QuoteHandler {
 	public Mono<ServerResponse> echo(ServerRequest request) {
 		return ok().contentType(TEXT_PLAIN)
 				.body(request.bodyToMono(String.class), String.class);
+	}
+
+	public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+		return ok()
+				.contentType(APPLICATION_STREAM_JSON)
+				.body(this.quoteStream, Quote.class);
+	}
+
+	public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+		int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+		return ok()
+				.contentType(APPLICATION_JSON)
+				.body(this.quoteStream.take(size), Quote.class);
 	}
 }
--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
@@ -6,6 +6,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
@@ -19,6 +21,8 @@ public class QuoteRouter {
 	public RouterFunction<ServerResponse> route(QuoteHandler quoteHandler) {
 		return RouterFunctions
 				.route(GET("/hello").and(accept(TEXT_PLAIN)), quoteHandler::hello)
-				.andRoute(POST("/echo").and(accept(TEXT_PLAIN).and(contentType(TEXT_PLAIN))), quoteHandler::echo);
+				.andRoute(POST("/echo").and(accept(TEXT_PLAIN).and(contentType(TEXT_PLAIN))), quoteHandler::echo)
+				.andRoute(GET("/quotes").and(accept(APPLICATION_JSON)), quoteHandler::fetchQuotes)
+				.andRoute(GET("/quotes").and(accept(APPLICATION_STREAM_JSON)), quoteHandler::streamQuotes);
 	}
 }
